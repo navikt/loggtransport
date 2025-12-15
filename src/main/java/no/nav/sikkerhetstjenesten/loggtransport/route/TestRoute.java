a715651ab4a68844f1ea4f97445a81cdac064609
Anine Andresen <anine.andresen@nav.no>
2025-12-15 08:40:04 +0100

    Hello world

--- src/main/java/no/nav/sikkerhetstjenesten/loggtransport/route/TestRoute.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggtransport/route/TestRoute.java
@@ -2,12 +2,14 @@ package no.nav.sikkerhetstjenesten.loggtransport.route;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TestRoute extends RouteBuilder {
 
     @Override
     public void configure() {
-        from("timer:test?period=60000")
+        from("timer://simpleTimer?period=600000")
                 .log("Hello World");
     }
 }
