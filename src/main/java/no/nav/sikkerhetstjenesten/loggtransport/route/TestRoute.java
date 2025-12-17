635d6d925dab9076cdded59b6ce5feeaf354c886
Anine Andresen <anine.andresen@nav.no>
2025-12-17 14:12:03 +0100

    Check

--- src/main/java/no/nav/sikkerhetstjenesten/loggtransport/route/TestRoute.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggtransport/route/TestRoute.java
@@ -9,9 +9,9 @@ public class TestRoute extends RouteBuilder {
     @Override
     public void configure() {
         from("timer://simpleTimer?period=600000")
-                .log("Hello World")
-                .setBody(simple("SELECT * FROM TESTLOGGER"))
-                .to("spring-jdbc:default")
-                .log("Message ${body}");
+                .log("Hello World");
+                //.setBody(simple("SELECT * FROM TESTLOGGER"))
+                //.to("spring-jdbc:default")
+                //.log("Message ${body}");
     }
 }
