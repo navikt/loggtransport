8e3c6a772e8ffef8f7763896614f3effc868b2a0
Anine Andresen <anine.andresen@nav.no>
2026-01-05 15:29:53 +0100

    Quartz for Spring Boot

--- src/main/java/no/nav/sikkerhetstjenesten/loggtransport/route/TestRoute.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggtransport/route/TestRoute.java
@@ -8,7 +8,10 @@ public class TestRoute extends RouteBuilder {
 
     @Override
     public void configure() {
-        from("timer://simpleTimer?period=600000")
+        //from("timer://simpleTimer?period=600000") // 10 minutter
+        //from("quartz://myGroup/myTimerName?cron=30+6+*+*+*+?")   // DB2 06:30 eller 7?
+        from("quartz://myGroup/myTimerName?cron=45+*+*+*+*+?")   // Oracle (kvart på) hver time
+                .setBody(constant("Test body"))
                 .log("Hello World");
     }
 }
