227a5ec70e3d4a863c276e3a68df1450dd70d79e
Anine Andresen <anine.andresen@nav.no>
2026-01-12 11:54:28 +0100

    Rydde

--- src/main/java/no/nav/sikkerhetstjenesten/loggtransport/route/TestRoute.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggtransport/route/TestRoute.java
@@ -2,17 +2,9 @@ package no.nav.sikkerhetstjenesten.loggtransport.route;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
-import org.springframework.context.annotation.Bean;
-import javax.sql.DataSource;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @Component
 public class TestRoute extends RouteBuilder {
-
-    @Bean(name = {"jdbcTemplate", "JdbcTemplate"})
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
-    }
 
     @Override
     public void configure() {
